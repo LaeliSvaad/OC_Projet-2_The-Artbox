c04b1cb3e64af3626a49893eb4ae04de5598a4fd
LaeliSvaad <ls@laelisvaad.com>
2025-01-31 20:21:22 +0100

    amélioration du code après mentorat: raccourcis PHP, séparation vue / contrôleur, allègement et lisibilité

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php 
     include("header.php");
-    include("oeuvres.php"); 
+    $oeuvres = include("oeuvres.php"); 
 ?>
-<div id="liste-oeuvres">
-    <?php foreach ($oeuvres as $oeuvre) {  ?>             
-    <article class="oeuvre">
-        <a href="oeuvre.php?id=<?= $oeuvre["id"] ?>">
-        <img src="img/<?= $oeuvre['img_src'] ?>" alt="<?= $oeuvre['title'] ?>">
-        <h2><?= $oeuvre['title'] ?></h2>
-        <p class="description"><?= $oeuvre['author'] ?></p>
-        </a>
-    </article>
-    <?php ; } ?>
+
+    <div id="liste-oeuvres">
+    <?php foreach ($oeuvres as $oeuvre):  ?>             
+        <article class="oeuvre">
+            <a href="oeuvre.php?id=<?= $oeuvre["id"] ?>">
+                <img src="img/<?= $oeuvre['img_src'] ?>" alt="<?= $oeuvre['title'] ?>">
+                <h2><?= $oeuvre['title'] ?></h2>
+                <p class="description"><?= $oeuvre['author'] ?></p>
+            </a>
+        </article>
+    <?php endforeach; ?>
     </div>
+
 <?php  include("footer.php"); ?>
--- oeuvre.php
+++ oeuvre.php
@@ -1,43 +1,41 @@
 <?php
-include("header.php");
+//Controller
+    $oeuvres = include("oeuvres.php");
 
-if(isset($_GET["id"])){ 
-    $id = $_GET["id"];
-    $o = null;
-    include("oeuvres.php");
+    //si "id" n'existe pas dans l'url, $id prend 0 pour valeur
+    $id = $_GET["id"] ?? 0;
 
+    //on initie cette variable pour gérer les erreurs par la suite
+    $oeuvre_existante= false;
+
+    //L'oeuvre est-elle dans le tableau? 
+    //Si oui, on la récupère dans $oeuvre et on passe $oeuvre_existante à true
     foreach($oeuvres as $oeuvre)
     {
         if($oeuvre["id"] == $id)
         {
-            $o = $oeuvre;
+            $oeuvre_existante = true;
+            break;
         }
     }
-
-    if($o != null){
-
-    
 ?>
-<article id="detail-oeuvre">
-    <div id="img-oeuvre">
-        <img src="img/<?= $o["img_src"] ?>" alt="<?= $o["title"] ?>">
-    </div>
-    <div id="contenu-oeuvre">
-        <h1><?= $o["title"] ?></h1>
-        <p class="description"><?= $o["author"] ?></p>
-        <p class="description-complete"><?= $o["description"] ?></p>
-    </div>
-</article>
-<?php 
-    }
-    else {
-        echo "<p>Erreur: cette oeuvre n'existe pas.</p>";
-    }
-} 
-else 
-{
-    echo "<p>Erreur: cette page n'existe pas.</p>";
-}
 
-include("footer.php");
-?>
+<!-- Vue -->
+    <?php include("header.php"); ?>
+
+    <?php if ($oeuvre_existante): ?>
+    <article id="detail-oeuvre">
+        <div id="img-oeuvre">
+            <img src="img/<?= $oeuvre["img_src"] ?>" alt="<?= $oeuvre["title"] ?>">
+        </div>
+        <div id="contenu-oeuvre">
+            <h1><?= $oeuvre["title"] ?></h1>
+            <p class="description"><?= $oeuvre["author"] ?></p>
+            <p class="description-complete"><?= $oeuvre["description"] ?></p>
+        </div>
+    </article>
+    <?php else: ?>
+        <p>Erreur: cette oeuvre n'existe pas.</p>
+    <?php endif; ?>
+
+    <?php include("footer.php"); ?>
